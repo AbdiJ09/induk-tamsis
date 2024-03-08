f4041bdee27963fe68343a812c53d8b47bd8daa5
Abdi J <abdij3196@gmail.com>
2024-03-09 00:40:44 +0700

    kelupaan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,9 +15,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    {{-- <link rel="stylesheet" href="/build/assets/app-B-gXXBrj.css"> --}}
-    {{-- <link rel="stylesheet" href="/build/assets/app-BA11tsjU.css"> --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="/build/assets/app-BEkac1vX.css">
+    <link rel="stylesheet" href="/build/assets/app-DephoxPn.css">
+    <script src="/build/assets/app-C-oGnuCb.js"></script>
     @livewireStyles
 </head>
 
@@ -36,8 +36,6 @@
     <!-- Footer -->
     <x-footer />
     <!-- End Footer -->
-
-    {{-- <script src="/build/assets/app-QW317hHE.js"></script> --}}
 
     @livewireScripts
 </body>
